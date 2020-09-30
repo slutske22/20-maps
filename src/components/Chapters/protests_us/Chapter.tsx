0e504deaf82fe66cd38f021db503bec8944f47c0
slutske22 <seth.lutske@gmail.com>
2020-09-29 18:10:46 -0700

    cleanup

--- src/components/Chapters/protests_us/Chapter.tsx
+++ src/components/Chapters/protests_us/Chapter.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import type {
-   Component,
-   FunctionComponent,
-   ReactNode,
-   ReactElement,
-} from 'react';
-import { MapProps } from '../../../types';
+import type { FunctionComponent } from 'react';
 
 import model from './model';
 import Template from '../../molecules/Chapter';
 import Map from './Map';
 
 const Chapter: FunctionComponent<{}> = () => {
-   return <Template fullWidthMap data={model} map={Map} />;
+	return <Template fullWidthMap data={model} map={Map} />;
 };
 
 export default Chapter;
