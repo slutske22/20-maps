9940cc053ccb4a5a895df18191c549c8c515c013
Seth Lutske <seth.lutske@gmail.com>
2020-10-01 17:11:16 +0000

    change Chapters to chapters

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,14 +10,14 @@ import Chapter2 from './chapters/protests_us/Chapter';
 import Chapter3 from './chapters/wildfires_aus/Chapter';
 
 const App = () => (
-	<div className="App">
-		<GlobalStyles />
-		<Header />
-		<Intro />
-		<Chapter1 />
-		<Chapter2 />
-		<Chapter3 />
-	</div>
+   <div className="App">
+      <GlobalStyles />
+      <Header />
+      <Intro />
+      <Chapter1 />
+      <Chapter2 />
+      <Chapter3 />
+   </div>
 );
 
 export default hot(module)(App);
