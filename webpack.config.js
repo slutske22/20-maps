51c4f517e9d1c03a95bacb70da4db89a0eee0cf4
Seth Lutske <c_Seth.Lutske@viasat.com>
2023-12-08 13:41:08 -1000

    Updating path

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		publicPath: '/',
+		publicPath: 'auto',
 	},
 	// target:'node', // messes with styled components.  why?
 	devtool: 'cheap-module-source-map',
